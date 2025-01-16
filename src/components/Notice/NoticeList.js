b94ae638de0a3ed144b639704f9cce29c81e126d
Lee-Song-Ha <songha1744@swu.ac.kr>
2025-01-16 14:21:16 +0900

    [song] domain 빠진 내용 수정& notice UI 수정

--- src/components/Notice/NoticeList.js
+++ src/components/Notice/NoticeList.js
@@ -13,10 +13,10 @@ const NoticeList = ({ notices }) => {
     <div className="notice-list">
       {/* 헤더 추가 */}
       <div className="notice-header flex items-center justify-between py-3 border-b-2 border-black font-bold text-center">
-        <div className="flex-shrink-0 w-10">번호</div>
+        <div className="flex-shrink-0 w-20">번호</div>
         <div className="flex-grow text-center">제목</div> {/* 가운데 정렬 */}
-        <div className="flex-shrink-0 w-32">작성일</div>
-        <div className="flex-shrink-0 w-20">작성자</div>
+        <div className="flex-shrink-0 w-64">작성일</div>
+        <div className="flex-shrink-0 w-32">작성자</div>
       </div>
       {notices.map((notice, index) => (
         <div
@@ -24,10 +24,10 @@ const NoticeList = ({ notices }) => {
           className="notice-item flex items-center justify-between py-3 border-b border-gray-300"
           onClick={() => handleNoticeClick(notice.id)}
         >
-          <div className="flex-shrink-0 w-10 text-center">{notice.id}</div>
+          <div className="flex-shrink-0 w-20 text-center">{notice.id}</div>
           <div className="flex-grow text-center truncate">{notice.title}</div> {/* 가운데 정렬 */}
-          <div className="flex-shrink-0 w-32 text-center">{notice.date}</div>
-          <div className="flex-shrink-0 w-20 text-center">{notice.author}</div>
+          <div className="flex-shrink-0 w-64 text-center">{notice.date}</div>
+          <div className="flex-shrink-0 w-32 text-center">{notice.author}</div>
         </div>
       ))}
     </div>
