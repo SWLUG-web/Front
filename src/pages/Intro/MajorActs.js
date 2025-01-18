78b6a93cdb433527c4ffec7d5e8bd3f926813f6e
mnzy412 <mnzy412@gmail.com>
2025-01-19 03:09:40 +0900

    [mnzy] 사진 정리

--- src/pages/Intro/MajorActs.js
+++ src/pages/Intro/MajorActs.js
@@ -8,7 +8,7 @@ const MajorActs = () => {
             cards: [
                 {
                     description: "리버싱, 시스템 해킹, 웹해킹, 포렌식 등 다양한 분야의 \n 심화 스터디를 운영하고 있어요.",
-                    image: "/study_image_8.png"
+                    image: "/study_image_1.png"
                 },
                 {
                     description: "스터디에 필요한 책, 강의 등은 동아리 자료를 대여해주며, 스터디에 필요한 운영비를 지원하고 있어요.",
@@ -25,7 +25,7 @@ const MajorActs = () => {
                 },
                 {
                     description: "프로젝트를 통해 논문 작성이나 대회, 공모전 등 대외활동에 적극적으로 참여할 수 있어요.",
-                    image: "/project_image_3.png"
+                    image: "/project_image_2.png"
                 }
             ]
         },
@@ -34,11 +34,11 @@ const MajorActs = () => {
             cards: [
                 {
                     description: "선후배간의 친목 및 멘토-멘티 활동을 통해 전공에 대해 \n 보다 전문적인 도움을 받을 수 있어요.",
-                    image: "/mento_image_3.png"
+                    image: "/mentor_image_1.png"
                 },
                 {
                     description: "선배님들이나 교수진, 기업 초청으로 진로, 실무 경험 등에 대해 다양한 특강을 진행해요.",
-                    image: "/mento_image_1.png"
+                    image: "/mentor_image_2.png"
                 }
             ]
         },
