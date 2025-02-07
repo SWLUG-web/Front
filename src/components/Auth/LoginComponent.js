a2d2642ab0b7d553010d235cb3022206f8621b1b
mnzy412 <mnzy412@gmail.com>
2025-02-08 05:51:24 +0900

    [mnzy] 수정 테스트

--- src/components/Auth/LoginComponent.js
+++ src/components/Auth/LoginComponent.js
@@ -6,6 +6,8 @@ import { loginSuccess, loginFailure, setLoading } from '../../slices/authSlice';
 import "../../styles/LoginComponent.css";
 import "../../styles/common.css";
 
+//확인
+
 function LoginComponent() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
