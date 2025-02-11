bd43b39a5146ac5a87bacc834b3c2a1d4a64ac63
mnzy412 <mnzy412@gmail.com>
2025-02-12 06:10:22 +0900

    .

--- src/components/Auth/LoginComponent.js
+++ src/components/Auth/LoginComponent.js
@@ -6,7 +6,7 @@ import { loginSuccess, loginFailure, setLoading } from '../../slices/authSlice';
 import "../../styles/LoginComponent.css";
 import "../../styles/common.css";
 
-//확인
+
 
 function LoginComponent() {
   const dispatch = useDispatch();
